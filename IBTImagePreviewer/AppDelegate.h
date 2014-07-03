6155cf96095ecc33c4db070231b28e973609d3a2
Xummer <xummer26@gmail.com>
2014-07-03 20:24:57 +0800

    Initial Commit

---
+++ IBTImagePreviewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IBTImagePreviewer
+//
+//  Created by Xummer on 7/3/14.
+//  Copyright (c) 2014 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IBTImagePreviewerTests/IBTImagePreviewerTests.m
@@ -0,0 +1,34 @@
+//
+//  IBTImagePreviewerTests.m
+//  IBTImagePreviewerTests
+//
+//  Created by Xummer on 7/3/14.
+//  Copyright (c) 2014 Xummer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IBTImagePreviewerTests : XCTestCase
+
+@end
+
+@implementation IBTImagePreviewerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
